c990fccd86aff6dc0d932128a984051bbb70e471
Iwan Rahman <1w3n.rahman@gmail.com>
2023-07-11 23:09:16 -0400

    Implement colour selection

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let squares = [];
 let size = 0;
-const grid = document.querySelector(".container");
+const grid = document.querySelector('.container');
 
 function generateGrid(newSize){
   resetGrid();
@@ -24,18 +24,34 @@ document.body.addEventListener('mouseup', () => clearHover());
 
 function setHover(){
   for(let i = 0 ; i < size; i++){
-    squares[i].addEventListener('mousemove',color);
+    squares[i].addEventListener('mousemove',colorSquare);
   }
 }
 
 function clearHover(){
   for(let i = 0; i < size; i++){
-    squares[i].removeEventListener('mousemove',color);
+    squares[i].removeEventListener('mousemove',colorSquare);
   }
 }
 
-function color(event){
-  event.target.style.backgroundColor = 'plum';
+function colorSquare(event){
+  event.target.style.backgroundColor = color;
 }
+
+let colors = document.querySelectorAll('.color');
+let color = 'plum';
+
+//Iterate through all colour divs except for the custom color
+for(let i = 0; i <colors.length - 1;i++){
+  colors[i].addEventListener('click',(e) => {
+    color = e.target.style.backgroundColor
+  })
+}
+
+document.querySelector('.color input').addEventListener('input',(e) => {
+  color = document.querySelector('.color input').value;
+})
+
+
 
 generateGrid(256);
